b7ac149dc912be1086e58453c306ae700b77caa8
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2020-10-24 22:17:05 +0300

    Created useStockValue hook

---
+++ src/hooks/useStockValue.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import get from 'lodash/get';
+
+import { Stock } from './useStock';
+import { useStockContext } from './useStockContext';
+
+export const useStockValue = <V, T extends object = object>(path: string, stock?: Stock<T>): V => {
+    if (!stock) {
+        stock = useStockContext<T>();
+    }
+
+    const { observe, stopObserving, values } = stock;
+
+    const [value, setValue] = useState(() => get(values.current, path));
+
+    useEffect(() => {
+        setValue(get(values.current, path));
+        const observer = (value: V) => setValue(value);
+        observe(path, observer);
+        return () => stopObserving(path, observer);
+    }, [path]);
+
+    return value;
+};
