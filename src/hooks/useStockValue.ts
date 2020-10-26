a3ad7b2ee8ff3cfefcf632f9848b988bf485888f
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2020-10-27 00:31:13 +0200

    Updated documentation for hooks and utilities

--- src/hooks/useStockValue.ts
+++ src/hooks/useStockValue.ts
@@ -4,7 +4,12 @@ import get from 'lodash/get';
 import { Stock } from './useStock';
 import { useStockContext } from './useStockContext';
 
-/** Hook, which returns actual stock value. */
+/**
+ * Hook, which returns *actual* stock value.
+ * This means, it will update component each time when value in stock changes.
+ * @param path  - path to variable in stock, deeply gets value. @see https://lodash.com/docs/4.17.15#get
+ * @param stock - optional parameter, if you want to work with custom stock, not received from context.
+ */
 export const useStockValue = <V, T extends object = object>(path: string, stock?: Stock<T>): V => {
     if (!stock) {
         // eslint-disable-next-line react-hooks/rules-of-hooks
--- src/utils/useLazyRef.ts
+++ src/utils/useLazyRef.ts
@@ -1,5 +1,9 @@
 import { MutableRefObject, useRef } from 'react';
 
+/**
+ * Same as `React.useRef` but with lazy initialization.
+ * @param initializer - function, which initializes value. Called once.
+ */
 export const useLazyRef = <T>(initializer: () => T): MutableRefObject<T> => {
     const ref = useRef<T | null>(null);
 
