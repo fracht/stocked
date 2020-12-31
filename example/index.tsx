5d509be80903957334d6e954b6fc494d46d2bc05
Aleksandras Šukelovič <alex.shukel@gmail.com>
2020-12-31 13:04:59 +0200

    Created ProxyExample

--- example/index.tsx
+++ example/index.tsx
@@ -2,11 +2,13 @@ import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Example1 } from './components/Example1';
+import { ProxyExample } from './components/ProxyExample';
 
 const App = () => {
     return (
         <div>
-            <Example1 />
+            {/* <Example1 /> */}
+            <ProxyExample />
         </div>
     );
 };
