125f90ecebc131286b3cd2ad1892b5d93adec5fd
Aleksandras Šukelovič <alex.shukel@gmail.com>
2021-01-03 20:07:18 +0200

    Added return type in useAllStockValues type

--- src/hooks/useAllStockValues.ts
+++ src/hooks/useAllStockValues.ts
@@ -3,6 +3,6 @@ import { StockProxy } from '../typings/StockProxy';
 import { useStockValue } from './useStockValue';
 import { ROOT_PATH } from './useObservers';
 
-export const useAllStockValues = <T extends object = object>(customStock?: Stock<T>, proxy?: StockProxy) => {
+export const useAllStockValues = <T extends object = object>(customStock?: Stock<T>, proxy?: StockProxy): T => {
     return useStockValue((ROOT_PATH as unknown) as string, customStock, proxy);
 };
