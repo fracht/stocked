e9a0e6b43b0c8735fe4ff9d33a278bcb9ae4fd23
Artiom Tretjakovas <44021713+ArtiomTr@users.noreply.github.com>
2021-08-13 16:46:21 +0300

    Exported intercept function & remove internal entry (#68)

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export * from './hooks';
 export * from './typings';
 export * from './components';
 export * from './utils/ObserverArray';
+
+export { intercept } from './utils/useInterceptors';
--- src/internal.ts
+++
@@ -1,4 +0,0 @@
-export * from './utils/ObserverArray';
-export * from './utils/pathUtils';
-export * from './utils/useInterceptors';
-export * from './utils/useLazyRef';
