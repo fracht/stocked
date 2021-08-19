87ca32544ee0dee52cbed843711b1647ce5b7a6b
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-08-19 10:17:21 +0300

    Added origin path into batch updates

--- src/typings/BatchUpdate.ts
+++ src/typings/BatchUpdate.ts
@@ -1,7 +1,11 @@
+import { ROOT_PATH } from '..';
+
 /** Object, in which "stocked" calls observers */
 export type BatchUpdate<T> = {
     /** which paths should be updated */
     paths: string[];
+    /** path, which triggered subtree update */
+    origin: string | typeof ROOT_PATH;
     /** all values, which should be sent to observers */
     values: T;
 };
