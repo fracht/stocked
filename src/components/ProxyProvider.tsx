571b571faa472a72174d34a968028f54eb8bc902
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-11-05 16:46:49 +0200

    Fixed proxy props type

--- src/components/ProxyProvider.tsx
+++ src/components/ProxyProvider.tsx
@@ -5,16 +5,16 @@ import { StockContext } from './StockContext';
 import { useStockContext } from '../hooks';
 import { StockProxy } from '../typings';
 
-export type ProxyProviderProps = PropsWithChildren<{
-    proxy: StockProxy<unknown>;
+export type ProxyProviderProps<V> = PropsWithChildren<{
+    proxy: StockProxy<V>;
 }>;
 
-export const ProxyProvider = ({ proxy, children }: ProxyProviderProps) => {
+export const ProxyProvider = <V,>({ proxy, children }: ProxyProviderProps<V>) => {
     const stock = useStockContext();
 
     return (
         <StockContext.Provider value={stock}>
-            <ProxyContext.Provider value={proxy}>{children}</ProxyContext.Provider>
+            <ProxyContext.Provider value={proxy as StockProxy<unknown>}>{children}</ProxyContext.Provider>
         </StockContext.Provider>
     );
 };
