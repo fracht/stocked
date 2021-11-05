bb3d0d2becabfd85ae4f38adb488930064e85661
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-11-05 16:49:27 +0200

    Added possibility to specify function for children in ProxyProvider

--- src/components/ProxyProvider.tsx
+++ src/components/ProxyProvider.tsx
@@ -1,20 +1,24 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
+import { Pxth } from 'pxth';
 
 import { ProxyContext } from './ProxyContext';
 import { StockContext } from './StockContext';
 import { useStockContext } from '../hooks';
 import { StockProxy } from '../typings';
 
-export type ProxyProviderProps<V> = PropsWithChildren<{
+export type ProxyProviderProps<V> = {
     proxy: StockProxy<V>;
-}>;
+    children: React.ReactNode | ((path: Pxth<V>) => React.ReactNode);
+};
 
 export const ProxyProvider = <V,>({ proxy, children }: ProxyProviderProps<V>) => {
     const stock = useStockContext();
 
     return (
         <StockContext.Provider value={stock}>
-            <ProxyContext.Provider value={proxy as StockProxy<unknown>}>{children}</ProxyContext.Provider>
+            <ProxyContext.Provider value={proxy as StockProxy<unknown>}>
+                {typeof children === 'function' ? children(proxy.path) : children}
+            </ProxyContext.Provider>
         </StockContext.Provider>
     );
 };
